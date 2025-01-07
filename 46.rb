77e5258529002f7a00bec16f0beb5e56df49dca9
shin9898 <shintech9898@gmail.com>
2025-01-07 18:40:56 +0900

    模範解答及び振り返り

--- 46.rb
+++ 46.rb
@@ -9,6 +9,7 @@
 # ・シャッフルした占い結果を格納した配列から、上記の数値の順番の値を取り出す
 # 　["凶","中吉","吉", "大吉"]
 
+#回答
 def birthday_oracle(birthday)
   fortune = ["凶","中吉","吉", "大吉"]
   num =  (birthday * rand(9)) % 4
@@ -18,3 +19,17 @@ end
 puts "誕生日を入力してください。4月4日の場合の入力例：0404"
 birthday = gets.to_i
 birthday_oracle(birthday)
+
+# 模範解答
+def result_of_uranai(birthday)
+  results = ["凶", "中吉", "吉", "大吉"].shuffle
+  num = birthday * rand(10) % 4
+  puts "今日のあなたの運勢は、#{results[num]}だよ！"
+end
+
+puts "誕生日を入力してください！"
+
+birthday = gets.to_i
+result_of_uranai(birthday)
+
+# rand()の指定が間違えていた。回答のrandでは0~8のいずれかの数値での掛け算となってしまう。
